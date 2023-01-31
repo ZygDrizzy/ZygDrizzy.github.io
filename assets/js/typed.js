1825ce4a54874db1e0f99b30cd9168ad8edd8f72
ZygRunsYou <97956760+ZygDrizzy@users.noreply.github.com>
2023-01-31 10:08:22 +0800

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'Zyg#5213','/nolifers','/uiq','bootaeaters up','best dev','skid4life','paster',],
+  typeSpeed : 40,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
