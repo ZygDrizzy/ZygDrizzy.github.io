33d822e3829d445d132671e61532814d7397b425
ZygRunsYou <97956760+ZygDrizzy@users.noreply.github.com>
2023-03-11 13:31:56 +0800

    Delete assets directory

--- assets/js/typed.js
+++
@@ -1,7 +0,0 @@
-new Typed('#typed',{
-  strings : [,'Zyg#5213','/nolifers','/uiq','bootaeaters up','best dev','skid4life','paster',],
-  typeSpeed : 40,
-  delaySpeed : 90,
-  showCursor: false,
-  loop : true
-})
