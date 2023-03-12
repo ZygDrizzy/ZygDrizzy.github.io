1c2529d86d050a81f63e208c1fdc25510fd4c56e
ZygRunsYou <97956760+ZygDrizzy@users.noreply.github.com>
2023-03-12 19:48:28 +0800

    Update typed.js

--- zygassets/js/typed.js
+++ zygassets/js/typed.js
@@ -1,5 +1,5 @@
 new Typed('#typed',{
-  strings : [,'bootyeaters','top 1','shhhhh','shin','void','zen','oxx','we up'],
+  strings : [,'uiq','top 1','shhhhh','shin','void','zen','oxx','dom','we up'],
   typeSpeed : 35,
   delaySpeed : 90,
   showCursor: false,
